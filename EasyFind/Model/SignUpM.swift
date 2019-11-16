56a2790a67fd1a0619934035cfcac827c978fc2a
nathanjaswal <nathanjaswal@gmail.com>
2019-11-16 14:17:57 -0500

    update

---
+++ EasyFind/Model/SignUpM.swift
@@ -0,0 +1,15 @@
+//
+//  SignUPM.swift
+//  EasyFind
+//
+//  Created by Nitin on 16/11/19.
+//  Copyright © 2019 Ramanpreet Singh. All rights reserved.
+//
+
+import Foundation
+
+struct SignUpM : Codable {
+    let userName : String?
+    let password : String?
+    
+}
