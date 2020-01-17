46065769f04eab404c79f0f51286977dab363eb3
Nitin Jaswal <nathanjaswal@gmail.com>
2020-01-17 18:02:41 -0500

    [Nitin] keyboard manager added

--- EasyFind/AppDelegate.swift
+++ EasyFind/AppDelegate.swift
@@ -7,12 +7,16 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        /// keyboard_manager...
+        IQKeyboardManager.shared.enable = true
         
         sleep(4)
     
